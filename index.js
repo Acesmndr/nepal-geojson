bcab76bf6b21718f67df976eeeacce42b84f1a2e
acesmndr <acesmndr@gmail.com>
2018-02-18 15:59:21 +0545

    Updated to 77 districts with province data

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const fs = require('fs');
 const GeojsonMinifier = require('geojson-minifier');
 
 const NEPAL_GEOJSON = JSON.parse(fs.readFileSync(`${__dirname}/assets/nepal.geojson.packed`));
-const _roundOff = value => Math.floor(value * 1000000) / 1000000;
+const roundOff = value => Math.floor(value * 1000000) / 1000000;
 const nepalGeojson = {
   districts() {
     const minifyr = new GeojsonMinifier({ precision: 6 });
     const unpackedCoordinates = JSON.parse(minifyr.unpack(NEPAL_GEOJSON));
-    for (let i = 0; i < 75; i += 1) {
-      unpackedCoordinates.features[i].geometry.coordinates[0][0][0] = _roundOff(unpackedCoordinates.features[i].geometry.coordinates[0][0][0]);
-      unpackedCoordinates.features[i].geometry.coordinates[0][0][1] = _roundOff(unpackedCoordinates.features[i].geometry.coordinates[0][0][1]);
+    const numberOfDistricts = unpackedCoordinates.features.length;
+    for (let i = 0; i < numberOfDistricts; i += 1) {
+      unpackedCoordinates.features[i].geometry.coordinates[0][0][0] = roundOff(unpackedCoordinates.features[i].geometry.coordinates[0][0][0]);
+      unpackedCoordinates.features[i].geometry.coordinates[0][0][1] = roundOff(unpackedCoordinates.features[i].geometry.coordinates[0][0][1]);
     }
     return unpackedCoordinates;
   },
